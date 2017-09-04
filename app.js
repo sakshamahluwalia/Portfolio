32b94a06548554186d7de6076cd543dafdea0f25
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2017-09-04 08:06:38 +0000

    intial commit

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
+app.get("/projects", function(req, res){
+    res.render("projects(main)");
+});
+
+app.get("/projects/page2", function(req, res){
+    res.render("projects[1]");
+});
+
+app.get("/projects/page3", function(req, res){
+    res.render("projects[2]");
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server Up");
 });
