1d8901bb957ca789537ae614195b24742eea60d8
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2018-09-02 17:11:05 +0000

    Used github api to get projects

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express     = require("express"),
     app         = express(),
     projectDB    = require("./seed"),
+    GitHub      = require("github-api"),
     bodyParser  = require("body-parser");
+    
+var github = new GitHub({
+   token: '16435adee78fe16dc05a0c31e3351f78b4243042' 
+});
 
 app = express();
 app.set("view engine", "ejs");
@@ -9,8 +14,10 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.render("home", {projects: projectDB});
-});
+    github.getUser().listRepos({}, function(err, data, headers) {
+        res.render("home", {repos: data});
+    });
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server Up");
